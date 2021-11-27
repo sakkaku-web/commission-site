1daf35c6ed25a1728a29e43e34418d75df7f89d8
myin <minyin142p@gmail.com>
2021-11-27 12:30:48 +0100

    rename art-commissions

--- apps/art-commission/jest.config.js
+++ apps/art-commission/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  displayName: 'art-commision',
+  displayName: 'art-commission',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/art-commision',
+  coverageDirectory: '../../coverage/apps/art-commission',
 };
--- apps/art-commission/tailwind.config.js
+++ apps/art-commission/tailwind.config.js
@@ -1,4 +1,4 @@
-const {join} = require('path');
+const { join } = require('path');
 const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
 
 module.exports = {
@@ -15,4 +15,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
