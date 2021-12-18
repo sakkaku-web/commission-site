40ee3507bf500f3a1c4733c5b56b932ebf3bac4b
myin <minyin142p@gmail.com>
2021-12-18 12:35:50 +0100

    refactor: aws, move contact notification to own file

---
+++ apps/contact-cloud/src/stacks/contact-stack.ts
@@ -0,0 +1,46 @@
+import { Runtime, Code, Function } from '@aws-cdk/aws-lambda';
+import { RetentionDays } from '@aws-cdk/aws-logs';
+import { Effect, PolicyStatement } from '@aws-cdk/aws-iam';
+import { Topic } from '@aws-cdk/aws-sns';
+import { LambdaSubscription } from '@aws-cdk/aws-sns-subscriptions';
+import { Construct } from '@aws-cdk/core';
+import { join } from 'path';
+
+export function setupContactNotification(scope: Construct, libsPath: string) {
+  const contactFunction = new Function(scope, 'commissionContact', {
+    runtime: Runtime.NODEJS_14_X,
+    code: Code.fromAsset(join(libsPath, 'libs/commission-contact')),
+    handler: 'commission-contact.handler',
+    logRetention: RetentionDays.ONE_MONTH,
+  });
+
+  const topic = new Topic(scope, 'artSubmission', {
+    displayName: 'Art Submission Contact',
+    topicName: 'artSubmission',
+  });
+
+  const snsPublish = new PolicyStatement({
+    effect: Effect.ALLOW,
+    actions: ['sns:Publish'],
+    resources: [topic.topicArn],
+  });
+  contactFunction.addToRolePolicy(snsPublish);
+
+  const discordSubFunction = new Function(scope, 'discordSub', {
+    runtime: Runtime.NODEJS_14_X,
+    code: Code.fromAsset(join(libsPath, 'libs/discord-sub')),
+    handler: 'discord-sub.handler',
+    logRetention: RetentionDays.ONE_MONTH,
+  });
+
+  const ssmDiscordRead = new PolicyStatement({
+    effect: Effect.ALLOW,
+    actions: ['ssm:GetParameter'],
+    resources: ['arn:aws:ssm:eu-central-1:841152197398:parameter/discord_*'],
+  });
+  discordSubFunction.addToRolePolicy(ssmDiscordRead);
+
+  topic.addSubscription(new LambdaSubscription(discordSubFunction));
+
+  return { contactFunction };
+}
